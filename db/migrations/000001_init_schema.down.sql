cdcd072cd3d17e15b6ca992ec9be73904e10df76
wpcodevo <ziddah.edem@gmail.com>
2022-09-15 14:27:59 +0000

    updated

---
+++ db/migrations/000001_init_schema.down.sql
@@ -0,0 +1,13 @@
+CREATE TABLE
+    "posts" (
+        "id" UUID NOT NULL DEFAULT (uuid_generate_v4()),
+        "title" VARCHAR NOT NULL,
+        "category" VARCHAR NOT NULL,
+        "content" VARCHAR NOT NULL,
+        "image" VARCHAR NOT NULL,
+        "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" TIMESTAMP(3) NOT NULL,
+        CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+    );
+
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
